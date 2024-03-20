d860c57771c52f92d4302539404a6af55569b162
sofiakwok <zazakwok@gmail.com>
2024-03-20 13:50:03 -0400

    gets pose and orientation data

--- build/dg_optitrack/build/lib/dg_optitrack/subscriber.py
+++ build/dg_optitrack/build/lib/dg_optitrack/subscriber.py
@@ -3,22 +3,40 @@ from rclpy.node import Node
 
 import numpy as np
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from mocap4r2_msgs.msg import RigidBodies
+
+# RigidBody data specs:
+# std_msgs/Header header
+# uint32 frame_number
+# mocap4r2_msgs/RigidBody[] rigidbodies
+
+#Biped: streaming ID 1049
+# rigid_body publishes information type [mocap4r2_msgs/msg/RigidBodies]
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
         self.last_pose = np.array([0, 0, 0, 0, 0, 0, 0])
+        self.streaming_id = "1049"
         self.subscription = self.create_subscription(
-            Pose,
-            'mocap4r2_optitrack_driver',
+            RigidBodies,
+            'rigid_bodies',
             self.listener_callback,
             10)  #queue list
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        self.last_pose = msg.data
+        #self.get_logger().info('I heard: "%s"' % msg.rigidbodies)
+        for body in msg.rigidbodies:
+            if body.rigid_body_name == self.streaming_id:
+                position = np.array([body.pose.position.x, body.pose.position.y, body.pose.position.z])
+                orientation = np.array([body.pose.orientation.x, body.pose.orientation.y, body.pose.orientation.z, body.pose.orientation.w])
+                # self.get_logger().info('position: "%s"' % position)
+                # self.get_logger().info('orientation: "%s"' % orientation)
+                self.last_pose = np.concatenate((position, orientation), axis=None)
 
     def signal(self):
         return self.last_pose
@@ -46,6 +64,7 @@ class MinimalSubscriber(Node):
 # rclpy.Node.Subscriber('/mocap4r2_optitrack_driver/RigidBody/pose', Pose, some_callback)
 
 def main(args=None):
+    print("initializing")
     rclpy.init(args=args)
 
     minimal_subscriber = MinimalSubscriber()
--- build/dg_optitrack/prefix_override/sitecustomize.py
+++ build/dg_optitrack/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sofia/subscriber/install/dg_optitrack'
+sys.prefix = sys.exec_prefix = '/home/sofia/dg_optitrack/install/dg_optitrack'
