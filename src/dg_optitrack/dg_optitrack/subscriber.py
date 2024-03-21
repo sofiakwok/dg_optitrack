c0166572f5984f900051d6867966c7ad393dda4d
sofiakwok <zazakwok@gmail.com>
2024-03-21 17:47:12 -0400

    running with ros foxc

--- src/dg_optitrack/dg_optitrack/subscriber.py
+++ src/dg_optitrack/dg_optitrack/subscriber.py
@@ -19,7 +19,7 @@ class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.last_pose = np.array([0, 0, 0, 0, 0, 0, 0])
+        self.last_pose = np.array2string(np.array([0, 0, 0, 0, 0, 0, 0]), formatter={'float_kind':lambda x: "%.2f" % x})
         self.streaming_id = "1049"
         self.subscription = self.create_subscription(
             RigidBodies,
@@ -33,10 +33,9 @@ class MinimalSubscriber(Node):
         for body in msg.rigidbodies:
             if body.rigid_body_name == self.streaming_id:
                 position = np.array([body.pose.position.x, body.pose.position.y, body.pose.position.z])
+                # quaternion - x y z w
                 orientation = np.array([body.pose.orientation.x, body.pose.orientation.y, body.pose.orientation.z, body.pose.orientation.w])
-                # self.get_logger().info('position: "%s"' % position)
-                # self.get_logger().info('orientation: "%s"' % orientation)
-                self.last_pose = np.concatenate((position, orientation), axis=None)
+                self.last_pose = np.array2string(np.concatenate((position, orientation), axis=None))
 
     def signal(self):
         return self.last_pose
@@ -48,18 +47,6 @@ class MinimalSubscriber(Node):
     def vel_filter(self):
         return None
 
-    def main(args=None):
-        rclpy.init(args=args)
-
-        minimal_subscriber = MinimalSubscriber()
-
-        rclpy.spin(minimal_subscriber)
-
-        # Destroy the node explicitly
-        # (optional - otherwise it will be done automatically
-        # when the garbage collector destroys the node object)
-        minimal_subscriber.destroy_node()
-        rclpy.shutdown()
 
 # rclpy.Node.Subscriber('/mocap4r2_optitrack_driver/RigidBody/pose', Pose, some_callback)
 
